a0a28288f28ea623551c9286539c1cdd3964a74d
GéraudWilling <geraudwilling@hotmail.fr>
2020-11-07 21:24:56 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { registerRootComponent } from 'expo';
+import { useKeepAwake } from 'expo-keep-awake';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { StatusBar, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { NETWORK_INTERFACE } from './src/config';
+import { configureStore, Root } from './src/navigators/AppNavigator';
+
+
+StatusBar.setBarStyle('light-content', true);
+
+// Initialization of the Apollo (graphql) client
+const client = new ApolloClient({
+  link: new HttpLink({ uri: NETWORK_INTERFACE }),
+  cache: new InMemoryCache()
+})
 
 export default function App() {
+  useKeepAwake();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={configureStore({})}>
+      <ApolloProvider client={client}>
+        <Root />
+      </ApolloProvider>
+    </Provider>
   );
 }
+
+// AppRegistry.registerComponent('App', () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
--- ios/AirbnbClone/AppDelegate.h
+++ ios/AirbnbClone/AppDelegate.h
@@ -1,10 +1,14 @@
-#import <Foundation/Foundation.h>
-#import <EXUpdates/EXUpdatesAppController.h>
-#import <React/RCTBridgeDelegate.h>
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #import <UIKit/UIKit.h>
 
-#import <UMCore/UMAppDelegateWrapper.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate, EXUpdatesAppControllerDelegate>
+@property (nonatomic, strong) UIWindow *window;
 
 @end
